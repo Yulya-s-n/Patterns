5ecd86c91f7b586bec5f68915e43321110c97bc2
yuliya <yulya.s.n@bk.ru>
2023-05-16 14:37:25 +0300

    chang test

--- src/test/java/test/CardDeliveryTest.java
+++ src/test/java/test/CardDeliveryTest.java
@@ -36,11 +36,10 @@ public class CardDeliveryTest {
         $("[data-test-id=date] input").setValue(firstMeetingDate);
         $("[data-test-id=name] input").setValue(validUser.getName());
         $("[data-test-id=phone] input").setValue(validUser.getPhone());
-        $("[data-test-id=agreement]").click();
         $x("//span[contains(text(),'условиями')]").click();
         $x("//span[contains(text(),'Запланировать')]").click();
         $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(10))
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(5))
                 .shouldBe(Condition.visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
